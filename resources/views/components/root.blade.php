6aefa8cf79e675e857161cb689df7fd73852da49
Paul <paulsther@gmail.com>
2026-02-04 13:54:57 +0200

    project init

---
+++ resources/views/components/root.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flatstore - Find your perfect product</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="antialiased">
+    {{ $slot }}
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<x-root>
+    <div class="max-w-2xl mx-auto p-10">
+        <h1 class="text-4xl font-bold mb-6">Welcome to Our Application</h1>
+        <p class="text-lg mb-4">This is the welcome page of our application. Here you can find various features and
+            resources to get started.</p>
+    </div>
+</x-root>
