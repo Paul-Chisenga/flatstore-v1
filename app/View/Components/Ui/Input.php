e9ab83a0b99e428fea02131f4d81cb5e5ca14081
Paul <paul@Pauls-MacBook-Air.local>
2026-02-06 13:23:51 +0200

    input component init

---
+++ app/View/Components/Ui/Input.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components\Ui;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Input extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(public string $class = '')
+    {
+        //
+    }
+
+    function classes(): string
+    {
+        $base = "dark:bg-input/30 border-input focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 disabled:bg-input/50 dark:disabled:bg-input/80 h-8 rounded-lg border bg-transparent px-2.5 py-1 text-base transition-colors file:h-6 file:text-sm file:font-medium focus-visible:ring-3 aria-invalid:ring-3 md:text-sm file:text-foreground placeholder:text-muted-foreground w-full min-w-0 outline-none file:inline-flex file:border-0 file:bg-transparent disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50";
+        return trim(string: "{$base} {$this->class}");
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.ui.input');
+    }
+}
